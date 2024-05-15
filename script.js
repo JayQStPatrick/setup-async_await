c0954a80e9759ac976b157793c0b6a940cf7d1ce
JayQinbits <jqurix352@gmail.com>
2024-05-14 20:35:23 -0700

    asyn await

--- script.js
+++ script.js
@@ -3,24 +3,62 @@ const getPosition = function () {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 };
-const whereamI = async function () {
-  // Geolocation
-  const pos = await getPosition();
-  const { latitude: lat, longitude: lng } = pos.coords;
+const whereAmI = async function () {
+  try {
+    // Geolocation
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
 
-  // Reverse Coding
-  const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-  const dataGeo = await resGeo.json();
-  console.log(dataGeo);
+    // Reverse Coding
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    if (!resGeo.ok) throw new Error('Problem getting location data');
+    const dataGeo = await resGeo.json();
 
-  // Country data
-  const res = await fetch(
-    `https://restcountries.com/v3.1/alpha/${dataGeo.country}`
-  );
-  const data = await res.json();
-  console.log(data);
-  renderCountry(data[0]);
+    // Country data
+    const res = await fetch(
+      `https://restcountries.com/v3.1/alpha/${dataGeo.country}`
+    );
+    if (!res.ok) throw new Error('Problem getting country data');
+
+    const data = await res.json();
+    renderCountry(data[0]);
+    return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+  } catch (err) {
+    console.error(err);
+    renderError(`Something went wrong ${err.message}`);
+
+    // Reject Promise returned from async function
+    throw err;
+  }
 };
 
-whereamI();
-console.log('FIRST');
+// console.log('1: Will get Location');
+// whereAmI()
+//   .then(city => console.log(city))
+//   .catch(city => console.log(`2: ${err.message}`))
+//   .finally(() => console.log('3: Finished getting location'));
+
+(async function () {
+  try {
+    const city = await whereAmI();
+    console.log(`2: ${city}`);
+  } catch (err) {
+    console.error(`2: ${err.message}`);
+  }
+  console.log('3: Finished getting location');
+})();
+
+// Running Promises in parallel
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    const data = await Promise.all([
+      getJson(`https://restcountries.eu/rest/v2/name/${c1}`),
+      getJson(`https://restcountries.eu/rest/v2/name/${c2}`),
+      getJson(`https://restcountries.eu/rest/v2/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+get3Countries('portugal', 'canada', 'tanzania');
